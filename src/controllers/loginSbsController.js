85967a00d42491c36a3fef22764a8a0af6a9c87b
Kev <kevin.ramos@vcaperu.com>
2023-04-03 17:04:26 -0500

    add functions sendvalues laraigo

--- src/controllers/loginSbsController.js
+++ src/controllers/loginSbsController.js
@@ -1,7 +1,11 @@
 import sbsService from '../services/sbsService.js'
+import laraigoService from '../services/laraigoService.js'
 
 const getLogin = async (req, res, next) => {
     const token = req.token; 
+    const key = req.query.key;
+    const [corpid, orgid, conversationid, personid] = key.split('-');
+
     const { body } = req;
 
     if(!req.body.tipo_documento || !req.body.numero_documento || !req.body.contrasenia){
@@ -11,14 +15,32 @@ const getLogin = async (req, res, next) => {
         return next(error);
     }
 
+    if(!req.query.key){
+        const error = new Error("Parameter? 'key' can not be empty");
+        error.statusCode = 400;
+        error.code = 'missing_parameter';
+        return next(error);
+    }
+
     const data = {
         tipo_documento: body.tipo_documento,
         numero_documento: body.numero_documento,
         contrasenia: body.contrasenia
+
+    }
+
+    const valuesLaraigo = {
+        corpid: corpid,
+        orgid: orgid,
+        conversationid: conversationid,
+        personid: personid
     }
 
     try {
         const tokenSbs = await sbsService.getLogin(token, data);
+        const apiLaraigo = await laraigoService.sendValues(valuesLaraigo)
+        console.log(apiLaraigo);
+
         res.status(201).send({ data: tokenSbs });
 
     } catch (error) {
