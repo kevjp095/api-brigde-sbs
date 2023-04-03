58b669868c1103ef24695e25be533198add30aa0
Kev <kevin.ramos@vcaperu.com>
2023-04-03 08:50:41 -0500

    rename route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv';
-import v1TipoCambioRouter from './v1/routes/tipoCambio.js'
-import v1ReporteRouter from './v1/routes/reporte.js'
+import errorHandler from './middleware/errorHandler.js';
+import v1exchangeRouter from './v1/routes/exchangeRate.js'
+import v1ReportRouter from './v1/routes/report.js'
 import v1Login from './v1/routes/login.js'
 
 const app = express();
@@ -9,8 +10,10 @@ dotenv.config();
 const PORT = process.env.PORT || 3900;
 
 app.use(express.json());
-app.use("/api/v1/sbs/tipocambio", v1TipoCambioRouter)
-app.use("/api/v1/sbs/reporte", v1ReporteRouter)
+app.use(errorHandler);
+
+app.use("/api/v1/sbs/tipocambio", v1exchangeRouter)
+app.use("/api/v1/sbs/reporte", v1ReportRouter)
 app.use("/api/v1/sbs/login", v1Login)
 
 app.get("/", (req, res) => {
